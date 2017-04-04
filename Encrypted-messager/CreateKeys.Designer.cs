4edec09eec07a350a7645bfb5b37c635c8df3178
Unknown <chischis60@yahoo.es>
2017-04-05 00:19:11 +0200

    added pgp keys generation
    Using Bouncy Castle.
    Also, I will proably remove PgpSharp as it seems that adds too little funcionality. Bouncy Castle package does much more.

--- Encrypted-messager/CreateKeys.Designer.cs
+++ Encrypted-messager/CreateKeys.Designer.cs
@@ -29,18 +29,58 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(240, 192);
+            this.button1.Location = new System.Drawing.Point(12, 318);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(176, 132);
+            this.button1.Size = new System.Drawing.Size(659, 216);
             this.button1.TabIndex = 0;
             this.button1.Text = "Create keys";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(114, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(557, 30);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(114, 87);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(557, 30);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(8, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Full name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(12, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 23);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Password:";
             // 
             // CreateKeys
             // 
@@ -48,6 +88,10 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DimGray;
             this.ClientSize = new System.Drawing.Size(683, 546);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
@@ -55,11 +99,16 @@
             this.Text = "Create Keys";
             this.Load += new System.EventHandler(this.CreateKeys_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- Encrypted-messager/CreateKeys.cs
+++ Encrypted-messager/CreateKeys.cs
@@ -1,14 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using Encrypted_messager;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using PgpSharp.GnuPG;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Crypto.Generators;
+using Org.BouncyCastle.Security;
+using System.IO;
+using Org.BouncyCastle.Math;
+using Org.BouncyCastle.Bcpg.OpenPgp;
+using Org.BouncyCastle.Bcpg;
 
 namespace Encrypted_messager
 {
@@ -26,17 +26,81 @@ namespace Encrypted_messager
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Keys.GenerateKeys();
+            Keys.GenerateKeys(textBox1.Text, textBox3.Text);
+        }
+
+        public void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 
     public static class Keys
     {
-        public static void GenerateKeys()
+        public static void GenerateKeys(string fullname, string password)
         {
             GnuPGTool pgpsharp = new GnuPGTool();
-            pgpsharp.KeyringFolder = Environment.CurrentDirectory + "public-keyring.pkr";
-            
+            pgpsharp.KeyringFolder = Environment.CurrentDirectory + @"\Keys\";
+            Directory.CreateDirectory(Environment.CurrentDirectory + @"\Keys\");
+            GenerateKey(fullname, password, Environment.CurrentDirectory + @"\Keys\");
+
+        }
+
+        public static void GenerateKey(string username, string password, string keyStoreUrl)
+        {
+            IAsymmetricCipherKeyPairGenerator kpg = new RsaKeyPairGenerator();
+            kpg.Init(new RsaKeyGenerationParameters(new BigInteger((new SecureRandom()).Next(0, Int32.MaxValue).ToString()), new SecureRandom(), 4096, 20));
+            AsymmetricCipherKeyPair kp = kpg.GenerateKeyPair();
+
+            FileStream out1 = new FileInfo(string.Format("{0}secret.asc", keyStoreUrl)).OpenWrite();
+            FileStream out2 = new FileInfo(string.Format("{0}pub.asc", keyStoreUrl)).OpenWrite();
+
+            ExportKeyPair(out1, out2, kp.Public, kp.Private, username, password.ToCharArray(), true);
+
+        }
+
+        private static void ExportKeyPair(
+                Stream secretOut,
+                Stream publicOut,
+                AsymmetricKeyParameter publicKey,
+                AsymmetricKeyParameter privateKey,
+                string identity,
+                char[] passPhrase,
+                bool armor)
+        {
+            if (armor)
+            {
+                secretOut = new ArmoredOutputStream(secretOut);
+            }
+
+            PgpSecretKey secretKey = new PgpSecretKey(
+                PgpSignature.DefaultCertification,
+                PublicKeyAlgorithmTag.RsaGeneral,
+                publicKey,
+                privateKey,
+                DateTime.Now,
+                identity,
+                SymmetricKeyAlgorithmTag.Aes256,
+                passPhrase,
+                null,
+                null,
+                new SecureRandom()
+                );
+
+            secretKey.Encode(secretOut);
+            secretOut.Close();
+            if (armor)
+            {
+                publicOut = new ArmoredOutputStream(publicOut);
+            }
+            PgpPublicKey key = secretKey.PublicKey;
+            key.Encode(publicOut);
+            publicOut.Close();
         }
     }
 }
